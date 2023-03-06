0d0a1ba1f4c716dc8bd7f083a6e6ee62dafbc541
CicliGs <53313998+CicliGs@users.noreply.github.com>
2023-03-06 10:28:00 +0300

    Added LAB 1-2

---
+++ KPO_LAB1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    using namespace std;
+
+        int n, m, a;
+        unsigned long long result, del, del_2;
+        cin >> n;
+        cin >> m;
+        cin >> a;
+
+        del = n / a;
+        if (n % a != 0) 
+        {
+            del++;
+        }
+
+        del_2 = m / a;
+
+        if (m % a != 0) 
+        {
+            del_2++;
+        }
+        
+        result = del * del_2;
+        cout << result << endl;
+}
